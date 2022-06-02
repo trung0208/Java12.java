8eb32a1ed3962898b5c8a434387f801c1de7f529
trung0208 <105049958+trung0208@users.noreply.github.com>
2022-06-02 22:31:06 +0700

    update

---
+++ lesson1/src/Lesson1.java
@@ -0,0 +1,14 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        // sự khác biệt giữa ++i và i++ là i++ tăng giá trị i lên 1 và trả về giá trị mới,còn ++i cũng tăng giá trị i lên 1 nhưng trả về giá trị ban đầu i.
+        int i = 5;
+        int j = ++i;
+        System.out.println(i + "- " + j);
+
+
+
+
+
+
+    }
+}
