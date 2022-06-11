c8d94480347fb6b35ae4e243820e80b190d494a2
trung0208 <105049958+trung0208@users.noreply.github.com>
2022-06-11 18:15:58 +0700

    update

--- Ecersice/src/Ecercise5.java
+++ Ecersice/src/Ecercise5.java
@@ -7,14 +7,30 @@ public class Ecercise5 {
         int n = sc.nextInt();
         System.out.println("nhập số cột của mảng");
         int m = sc.nextInt();
-        int [][] array = new int[n][m];
-        for ( int i =0;i<n;i++){
-            for (int j=0;j<m;j++){
-                System.out.println("nhập phần tử thứ [" +i+","+j+"]");
-                array[i][j]= sc.nextInt();
+        int[][] array = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("nhập phần tử thứ [" + i + "," + j + "] :");
+                array[i][j] = sc.nextInt();
             }
         }
+        System.out.println("mảng in ra là");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        //liệt kê các phần tử trên đường chéo chính
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == j) {
+                    System.out.println("các phần tử nằm trên đường chéo chính là :" + array[i][j]);
+                }
+            }
 
+
+        }
 
     }
 }
